0ada91cba499ddafe9ca2a05ae2e14b80dd70774
BernardAttafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-12 12:38:32 +0000

    Update Equations - Used.py

--- pages/Equations - Used.py
+++ pages/Equations - Used.py
@@ -3,6 +3,13 @@ import streamlit as st
 import sympy as smp
 from sympy import*
 #setting up the variables
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.write('Syngas Balanced Equation')
 
 
