56c389431925d3260c108376a875aa6e156ef4f1
BernardAttafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-12 12:43:02 +0000

    Update Equations - Used.py

--- pages/Equations - Used.py
+++ pages/Equations - Used.py
@@ -6,6 +6,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
