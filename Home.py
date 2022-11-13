4a7a9f6c7b57b39c7a10090e5740d914f5cbb5f9
Bernard Attafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-13 00:01:22 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -32,34 +32,6 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-def set_page_title(title):
-    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
-        <iframe height=0 srcdoc="<script>
-            const title = window.parent.document.querySelector('title') \
-                
-            const oldObserver = window.parent.titleObserver
-            if (oldObserver) {{
-                oldObserver.disconnect()
-            }} \
-
-            const newObserver = new MutationObserver(function(mutations) {{
-                const target = mutations[0].target
-                if (target.text !== '{title}') {{
-                    target.text = '{title}'
-                }}
-            }}) \
-
-            newObserver.observe(title, {{ childList: true }})
-            window.parent.titleObserver = newObserver \
-
-            title.text = '{title}'
-        </script>" />
-    """)
-
-
-set_page_title("My new title")
-
-
 mystyle = '''
     <style>
         p {
