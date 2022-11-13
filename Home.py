c22a5594ef6ac154dfa1a68967625509bba1d7a5
Bernard Attafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-13 00:34:58 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -33,8 +33,8 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-	    .viewerBadge_link__1S137 {
-	    dispay: none;}										
+	    viewerBadge_link__1S137{
+	    visibility: hidden;}										
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
