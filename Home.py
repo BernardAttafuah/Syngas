ab59ed9adb41097f0c128ee532e1d12c82485640
Bernard Attafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-13 00:45:55 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -32,7 +32,7 @@ st.set_page_config(
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            
 	    viewerBadge_link__1S137{
 	    visibility: hidden;}										
             </style>
