7e89787481ac1bbf5a3c629affe1021fa4473009
Bernard Attafuah <107581993+BernardAttafuah@users.noreply.github.com>
2022-11-20 19:40:08 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ import sympy as smp
 from sympy import*
 import pandas as pd
 import numpy as np
+@st.cache
 
 st.set_page_config(
 
@@ -23,7 +24,7 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-@st.cache
+
 def streamlit_menu():
     if streamlit_menu:
         with st.sidebar:
